0e5819bb61e55cb44782d2cd8c7cb4bd197c0524
anna-dm <anna-anna-frolova@bk.ru>
2023-01-06 21:22:15 +0300

    features

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './feature.css';
 
+const Feature = ({ title, text }) => (
+    <div className="gpt3__features-container__feature">
+        <div className="gpt3__features-container__feature-title">
+            <div />
+            <h1>{title}</h1>
+        </div>
+        <div className="gpt3__features-container__feature-text">
+            <p>{text}</p>
+        </div>
+    </div>
+);
 
-const Feature = () => {
-    return (
-        <div>Feature</div>
-    )
-}
-
-export default Feature
+export default Feature;
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import Feature from '../../components/feature/Feature';
 import './whatGPT3.css';
 
 const WhatGPT3 = () => {
     return (
-        <div>WhatGPT3</div>
+        <div className='gpt3__whatgtp3 section__margin' id='whpt3'>
+            <div className='gpt3__whatgtp3-feature'>
+                <Feature title='What is GPT-3' text='We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.' />
+            </div>
+            <div className='gpt3__whatgtp3-heading'>
+                <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+                <p>Explore The Library</p>
+            </div>
+            <div className='gpt3__whatgtp3-container'>
+                <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. " />
+                <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+                <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+            </div>
+        </div>
     )
 }
 

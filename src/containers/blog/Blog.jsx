ab2d8811ffaca11b3b3766cc199a7c3d9e5644b4
anna-dm <anna-anna-frolova@bk.ru>
2023-01-09 22:22:44 +0300

    blog

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Article } from '../../components';
+import { blog01, blog02, blog03, blog04, blog05 } from './imports';
 import './blog.css';
 
 
@@ -12,13 +13,13 @@ const Blog = () => {
             </div>
             <div className='gpt3__blog-container'>
                 <div className='gpt3__blog-container_groupA'>
-                    <Article />
+                    <Article imgUrl={blog01} date="Sep 26, 2021" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
                 </div>
                 <div className='gpt3__blog-container_groupB'>
-                    <Article />
-                    <Article />
-                    <Article />
-                    <Article />
+                    <Article imgUrl={blog02} date="Sep 26, 2021" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+                    <Article imgUrl={blog03} date="Sep 26, 2021" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+                    <Article imgUrl={blog04} date="Sep 26, 2021" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+                    <Article imgUrl={blog05} date="Sep 26, 2021" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
                 </div>
             </div>
         </div>
